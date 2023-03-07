ba8613ddb38175731348e9a3ca7d1adfe85c27bd
Merdin <merdin10@icloud.com>
2023-03-07 02:41:24 +0100

    rename folder

--- expenses_app/lib/widgets/sections/user_transactions/new_transaction.dart
+++ expenses_app/lib/widgets/sections/user_transactions/new_transaction.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../../adaptives/adaptive_flat_button.dart';
+import '../../adaptive/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
